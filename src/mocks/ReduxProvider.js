025e5de9430c30fdff370a86bf6fd77a676cfa2a
Michael Sanabria <maicolsana12@gmail.com>
2022-04-20 07:48:38 -0500

    Merge with master branch

--- src/mocks/ReduxProvider.js
+++
@@ -1,8 +0,0 @@
-import { Provider } from "react-redux";
-import store from "../store/index";
-
-const ReduxProvider = ({ children }) => {
-  return <Provider store={store}> {children} </Provider>;
-};
-
-export default ReduxProvider;
